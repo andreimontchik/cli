f30b08a10f5f10f291a72d101e57a03751250049
Andrei Montchik <andrei@montchik.net>
2026-01-29 13:16:09 -0600

    Added more tokens

--- src/mints.ts
+++ src/mints.ts
@@ -19,10 +19,14 @@ const tokens: TokenMap = {
     PUMP: { mint: new PublicKey("pumpCmXqMfrsAkQ5r49WcJnRayYRqmXz6ae8H7H9Dfn"), decimals: 6 },
     mSOL: { mint: new PublicKey("mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So"), decimals: 9 },
     jitoSOL: { mint: new PublicKey("J1toso1uCk3RLmjorhTtrVwY9HJ7X8V9yYac6Y7kGCPn"), decimals: 9 },
+    dSOL: { mint: new PublicKey("Dso1bDeDjCQxTrWHqUUi63oBvV7Mdm6WaobLbQ7gnPQ"), decimals: 9 },
+    bSOL: {mint: new PublicKey("bSo13r4TkiE4KumL71LsHTPpL2euBYLFx6h9HP3piy1"), decimals: 9},
     HNT: { mint: new PublicKey("hntyVP6YFm1Hg25TN9WGLqM12b8TQmcknKrdu1oxWux"), decimals: 8 },
     CLOUD: { mint: new PublicKey("CLoUDKc4Ane7HeQcPpE3YHnznRxhMimJ4MyaUqyHFzAu"), decimals: 9 },
     JLP: { mint: new PublicKey("27G8MtK7VtTcCHkpASjSDdkWWYfoqT6ggEuKidVJidD4"), decimals: 6 },
     DRIFT: { mint: new PublicKey("DriFtupJYLTosbwoN8koMbEYSx54aFAVLddWsbksjwg7"), decimals: 6 },
+    W: { mint: new PublicKey("85VBFQZC9TZkfaptBWjvUw7YbZjy52A6mjtPGjstQAmQ"), decimals: 6 },
+    WBTC: {mint: new PublicKey("3NZ9JMVBmGAqocybic2c7LQCJScmgsAZ6vQqTDzcqmJh"), decimals: 8},
 };
 
 export function getToken(symbol: string): TokenInfo | undefined {
